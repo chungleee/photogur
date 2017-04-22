c52a62506274a2787f907da06dd6fbd40cda9154
Leon <leon.karho.chung@gmail.com>
2017-04-22 16:48:02 -0400

    setup validations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,6 @@
 class Picture < ApplicationRecord
-  
+  validates :artist, :url,  :presence => true
+  validates :title, length: { minimum: 3 }
+  validates :title, length: { maximum: 20 }
+  validates :url, uniqueness: true
 end
